a48a53ef204471c212a8bb921fdd318793409259
ashu199828 <93378896+ashu199828@users.noreply.github.com>
2022-04-11 13:51:37 +0530

    First commit Message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import MainComponent from "./components/MainComponent";
 import Nav from "./components/Nav";
-import data from './components/Data'
-import Meme from "./components/Meme";
+
 
 function App() {
   
-  const meme = data.map(item =>{
-    return <Meme
-    key={item.id}
-    img={item.url}
-    
-    
-    
-    />
-  })
   return (
     <div className="App">
       <Nav/>
       <MainComponent/>
-      {/* {meme} */}
+      
     </div>
   );
 }
